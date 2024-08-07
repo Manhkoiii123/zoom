be859cdb258d408bfd67931a9152df555d7a7e0d
padi-dev-manhtd <manhtd@paditech.com>
2024-08-07 23:19:43 +0700

    Create calls/meetings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import CreateMeetingPage from "@/app/CreateMeetingPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Meetings App",
+  description: "A video calling app",
+};
+
 export default function Home() {
-  return <></>;
+  return <CreateMeetingPage />;
 }
